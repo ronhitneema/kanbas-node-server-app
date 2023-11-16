2579b6c56b719cd6d72a1db8eaff899ca1d7feb4
ronhitneema <ronhitn@gmail.com>
2023-11-15 20:52:25 -0500

    added code for a5

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./Lab5.js";
+import cors from "cors";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
+
+const app = express();
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+);
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
